d7d246e3cae699e00d5d1297a139d39587268671
nice to meet you <qduyhoang@gmail.com>
2018-04-21 19:38:06 -0400

    Show current user's mail on nav drawer header

--- app/src/main/java/com/example/hello/alarm/MainActivity.java
+++ app/src/main/java/com/example/hello/alarm/MainActivity.java
@@ -4,14 +4,18 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -20,6 +24,9 @@ import android.widget.ListView;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.TimePicker;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -36,11 +43,51 @@ public class MainActivity extends AppCompatActivity {
     PendingIntent pending_intent;
     ArrayList alarm_data;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.context = this;
+
+        //Side nav drawer
+        final DrawerLayout drawer_layout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+
+        //Get information from current user, if there is one.
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        String name = "", email = "";
+        Uri photoUrl;
+
+        if (currentUser != null) {
+            name = currentUser.getDisplayName();
+            email = currentUser.getEmail();
+            photoUrl = currentUser.getPhotoUrl();
+        };
+        //Set information in the nav's header
+        View header_view = navigationView.getHeaderView(0);
+        TextView nav_user = header_view.findViewById(R.id.user_name);
+
+        nav_user.setText(email);
+
+
+        //On click listener for items in the nav drawer
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                // set item as selected to persist highlight
+                item.setChecked(true);
+                // close drawer when item is tapped
+                drawer_layout.closeDrawers();
+                return true;
+            }
+        });
+
+
+
+
         alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
         timePicker = findViewById(R.id.timePicker);
         //Create array of all alarm to show on the list view
